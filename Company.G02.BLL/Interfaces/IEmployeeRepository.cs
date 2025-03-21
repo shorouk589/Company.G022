9dcd77fcc30c7140568edbaa33281339ddfaf8bf
shorouk589 <shoroukashraf66@gmail.com>
2025-03-21 02:04:11 +0200

    Implementation Of Employee Service

---
+++ Company.G02.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using Company.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+
+        public IEnumerable<Employee> GetALL();
+
+        Employee? Get(int id);
+
+        int Add(Employee model);
+
+        int Update(Employee model);
+        int Delete(Employee model);
+    }
+}
