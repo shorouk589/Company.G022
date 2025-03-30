c984c7d6914d1a5768ab2af52ed43714926f3b98
shorouk589 <shoroukashraf66@gmail.com>
2025-03-30 10:32:34 +0200

    SearchInput

--- Company.G02.BLL/Repository/EmployeeRepository.cs
+++ Company.G02.BLL/Repository/EmployeeRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Company.G02.BLL.Interfaces;
 using Company.G02.DAL.Data.Contexts;
 using Company.G02.DAL.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -50,11 +51,24 @@ namespace Company.G02.BLL.Repository
         ///
 
 
-
+        private CompanyDbContext _Context { get; }
         public EmployeeRepository(CompanyDbContext context) : base(context)//ASK CLR Create Object From CompanyDbContext    
         {
+            _Context = context;
+        }
+
+
+
+        public List<Employee> GetByName(string name)
+        {
+            return _Context.Employees.Include(E => E.Department)
+                                      .Where(E => E.Name
+                                      .ToLower()
+                                      .Contains(name.ToLower()))
+                                      .ToList();
 
         }
+
 
     }
 }
